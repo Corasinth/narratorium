3cfafd4b8b78beabeb40a16058a6c8088c78cba1
Sean Oh <seanoh@ucsb.edu>
2022-08-04 14:01:24 -0700

    add controller indexes

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const homeRoutes = require("./home-routes");
+const apiRoutes = require("./api");
+
+const router = express.Router();
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;
