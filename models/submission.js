e358da17172791394ef592d48cb4110fd39e5d33
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-04 12:44:02 -0700

    added seed script to package.json, create models, seed db, download mysql

--- models/submission.js
+++ models/submission.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Story extends Model { }
+class Submission extends Model {}
 
-Story.init(
+Submission.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,22 +11,21 @@ Story.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    storyname: {
+    submission: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    submission_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'submission',
-        key: 'id',
-      },
-    },
-    //I think that we do not need this as it would create a many-to-many complexity that we do not need right now, but for scalability, will it hurt the project in the long run not having it?
-    user_id: { //not needed now but for a stretch, story will belong to a user
+    user_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
+        key: 'id',
+      },
+    },
+    story_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'story',
         key: 'id',
       },
     },
@@ -36,8 +35,8 @@ Story.init(
     timestamps: true,
     freezeTableName: true,
     underscored: true,
-    modelName: 'story',
+    modelName: 'submission',
   }
 );
 
-module.exports = Story;
+module.exports = Submission;
