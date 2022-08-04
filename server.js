d0b08c411bcb60323eb5c2dee2f7474987b9215d
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-04 14:01:19 -0500

    add basic socket event

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer,{});
+const io = new Server(httpServer,{
+    cors: {origin:'*'}
+});
 
 const PORT = process.env.PORT || 3001;
 
 // app.use(routes);
 
 io.on("connection", (socket) => {
-  // ...
+    console.log('Checking to see if a connection is active');
+    //placeholder event handling for testing -- change names later
+    socket.on('event', (event) => {
+        console.log(event)
+        io.emit('event', `Someone has triggered an event: ${event}`)
+    })
 });
 
 httpServer.listen(PORT, ()=>{
