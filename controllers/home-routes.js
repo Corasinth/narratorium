ee81689f9b88ff6feab69cb3bd544f61441fc2bb
Sean Oh <seanoh@ucsb.edu>
2022-08-04 14:12:28 -0700

    add pseudocode for home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const {User, Story, Submission} = require("../models");
+
+// "/" get route for homepage
+
+// "/login" get route for login page
+
+// "/signup" get route for signup page
+
+// "/story" get route for story page
+
+
+module.exports = router;
