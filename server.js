3ffaa28c78549d818a33ce29edc2f0c530f8678f
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-04 14:13:19 -0500

    add seqeulize sync to server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 // const routes = require('./controllers/index');
-
+const sequelize = require('./config/connection');
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer,{
@@ -17,11 +17,11 @@ io.on("connection", (socket) => {
     console.log('Checking to see if a connection is active');
     //placeholder event handling for testing -- change names later
     socket.on('event', (event) => {
-        console.log(event)
-        io.emit('event', `Someone has triggered an event: ${event}`)
+        console.log(event);
+        io.emit('event', `Someone has triggered an event: ${event}`);
     })
 });
 
-httpServer.listen(PORT, ()=>{
+sequelize.sync({force: false}).then(() => httpServer.listen(PORT, ()=>{
     console.log(`Now listening to port ${PORT}`)
-});
+}));
