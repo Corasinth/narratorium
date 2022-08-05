3c3367d0d20407b60ea488d9596d43ec5b517578
darmstrong <armstrong2579@gmail.com>
2022-08-05 09:03:56 -0700

    add minor style edits, center forms and text, change text size

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+const app = express();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 // const routes = require('./controllers/index');
 const sequelize = require('./config/connection');
-const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer,{
     cors: {origin:'*'}
@@ -13,14 +23,31 @@ const PORT = process.env.PORT || 3001;
 
 // app.use(routes);
 
-io.on("connection", (socket) => {
-    console.log('Checking to see if a connection is active');
-    //placeholder event handling for testing -- change names later
-    socket.on('event', (event) => {
-        console.log(event);
-        io.emit('event', `Someone has triggered an event: ${event}`);
-    })
+// io.on("connection", (socket) => {
+//     console.log('Checking to see if a connection is active');
+//     //placeholder event handling for testing -- change names later
+//     socket.on('event', (event) => {
+//         console.log(event);
+//         io.emit('event', `Someone has triggered an event: ${event}`);
+//     })
+// });
+
+app.get('/', async (req, res) => {
+    res.render('homepage');
 });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.get('/storypage', (req, res) => {
+    res.render('storypage');
+});
+  
 
 sequelize.sync({force: false}).then(() => httpServer.listen(PORT, ()=>{
     console.log(`Now listening to port ${PORT}`)
