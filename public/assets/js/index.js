cf26b83cef70628e0acb9721a1b75896dd39225d
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-05 15:40:56 -0500

    create clientside js to send and recieve data on socket

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,3 +5,22 @@ const socket = io('http://localhost:3001') || io('https://narratorium.herokuapp.
 socket.on('connect', () => {
     console.log(`Connected with socket id ${socket.id}`)
 })
+
+socket.on('displayStory', (string) => {
+    //Displays story from database on page
+    console.log(string);
+})
+
+socket.on('error', (error) => {
+    console.log(`%c Server returned the following error: ${error}, 'color:red;font-weight:500'`)
+})
+
+function onSubmit (submissionText, position, user, story) {
+    socket.emit('submission', submissionText, position, user)
+}
+
+function onDelete (wordDeleted, position) {
+    socket.emit('deletion', wordDeleted, position, story)
+}
+
+
