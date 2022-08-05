e98119e647350d7b5d63694f8b48c8e359894e3b
Sean Oh <seanoh@ucsb.edu>
2022-08-05 11:46:44 -0700

    import path in server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const path = require("path");
 
 // Import modules
 const routes = require('./controllers/index');
