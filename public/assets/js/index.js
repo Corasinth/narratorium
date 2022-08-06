47e13508970ce2969301a651a78e14423fb8fb88
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-05 20:06:43 -0500

    fix socket bugs

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,26 +6,32 @@ socket.on('connect', () => {
     console.log(`Connected with socket id ${socket.id}`)
 })
 
-socket.on('displayStory', (string) => {
+socket.on('displayStory', (data) => {
     //Displays story from database on page
-    console.log(string);
+    let storyString;
+    for (let entries of data.submissions) {
+        storyString += `${entries.submission} `
+    }
+    console.log(storyString)
 })
 
 socket.on('error', (error) => {
-    console.log(`%c Server returned the following error: ${error}, 'color:red;font-weight:500'`)
+    console.log(`%c Server returned the following error: ${error}`, 'color:red;font-weight:500')
+    console.log(error)
 })
+
 
 socket.on('testEvent', (data) => {
     console.table(data)
 })
 
 //Call this function when a user makes a submission
-function onSubmit (submissionText, position, user_id, story_id) {
+function onSubmit(submissionText, position, user_id, story_id) {
     socket.emit('submission', submissionText, position, user_id, story_id)
 }
 
 //Call this function when a user deletes a word
-function onDelete (word_id) {
+function onDelete(word_id) {
     socket.emit('deletion', word_id)
 }
 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require("path");
 // Import modules
 const routes = require('./controllers/index');
 const sequelize = require('./config/connection');
+const { Op } = require("sequelize");
 
 //Import models 
 const { User, Story, Submission } = require('./models/index');
@@ -55,6 +56,7 @@ app.use(routes);
 // Set up sockets
 io.on("connection", async (socket) => {
     console.log(socket.id)
+    let storyString = ""
     socket.on('viewStory', async (story_id) => {
         try {
             const storyData = await Story.findByPk(story_id, {
@@ -64,24 +66,23 @@ io.on("connection", async (socket) => {
                     order: [['position', 'ASC']]
                 }],
             });
-            //Add code to assemble story, current values are temporary
-
-            const testData = await Submission.findAll()
-            io.emit('testEvent', testData)
-
-            // io.emit('displayStory', storyData)
+            // const testData = await Submission.findAll()
+            // io.emit('testEvent', testData)
+            io.emit('displayStory', storyData)
         } catch (err) {
             io.emit('error', err)
         }
     })
     //Takes in title and creates a new story
-    socket.on('addStory', async (storyName) => {
+    socket.on('addStory', async (storyname, response) => {
         try{
-            const storyData = await Story.create(storyName);
+            const storyData = await Story.create({storyname});
             response({
                 status: storyData
             })
+            io.emit('displayStory', storyData)
         } catch (err) {
+            console.log(err)
             io.emit('error', err)
         }
     })
@@ -94,10 +95,14 @@ io.on("connection", async (socket) => {
                     id: story_id
                 }
             })
-
-            const testData = await Submission.findAll()
-            io.emit('testEvent', testData)
-
+            const storyData = await Story.findByPk(story_id, {
+                include: [{
+                    model: Submission,
+                    separate: true,
+                    order: [['position', 'ASC']]
+                }],
+            });
+            io.emit('displayStory', storyData)
             response({
                 status: newName
             });
@@ -119,17 +124,21 @@ io.on("connection", async (socket) => {
                 }
             });
             //For each word in the submission, creates a new table entry and an appropriate position
-            for (let content of submissionArray) {
+            for (let submission of submissionArray) {
                 //Check that the create parameters are correct
-                const submissionData = await Submission.create({ content, position, user_id, story_id });
+                const submissionData = await Submission.create({ submission, position, user_id, story_id });
                 position++
             }
-            
-            const testData = await Submission.findAll()
-            io.emit('testEvent', testData)
-
-            // io.emit('displayStory', something)
+            const storyData = await Story.findByPk(story_id, {
+                include: [{
+                    model: Submission,
+                    separate: true,
+                    order: [['position', 'ASC']]
+                }],
+            });
+            io.emit('displayStory', storyData)
         } catch (err) {
+            console.log(err)
             io.emit('error', err)
         };
     });
@@ -160,10 +169,14 @@ io.on("connection", async (socket) => {
                 }
             });
 
-            const testData = await Submission.findAll()
-            io.emit('testEvent', testData)
-
-            // io.emit('displayStory', storyString)
+            const storyData = await Story.findByPk(story_id, {
+                include: [{
+                    model: Submission,
+                    separate: true,
+                    order: [['position', 'ASC']]
+                }],
+            });
+            io.emit('displayStory', storyData)
         } catch (err) {
             io.emit('error', err)
         };
