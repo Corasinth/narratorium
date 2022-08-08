5dfadee320770fad8ec0f83138181d2490a41a90
Corasinth <beforeyouknowkindness@gmail.com>
2022-08-08 11:53:55 -0500

    clean up folder structure

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const loginHandler = async (e) => {
+  e.preventDefault();
+
+  const username_email = document.querySelector('#login-username').value.trim();
+  const password = document.querySelector('#login-password').value.trim();
+
+  if(username_email && password) {
+    const res = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({username_email, password}),
+      headers: {"Content-Type": "application/json"}
+    });
+    console.log(res);
+
+    if(res.ok) {
+      document.location.replace("/");
+    } else {
+      const body = await res.json();
+      const status = document.getElementById("status");
+      status.innerHTML = `<div>
+      <p>${body.message}</p>
+    </div>`
+    }
+  }
+}
+
+document.querySelector('#login-form').addEventListener('click', loginHandler);
