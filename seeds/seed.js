68ca8468a9c858b57a05762a0203fc5ed26d5ac9
Sean Oh <seanoh@ucsb.edu>
2022-08-09 19:24:58 -0700

    refactor seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,29 +1,32 @@
+// Import packages
 const sequelize = require('../config/connection');
 const { User, Submission, Story } = require('../models');
 
+// Import json files
 const userData = require('./userData.json');
 const submissionData = require('./submissionData.json');
 const storyData = require('./storyData.json');
 
+// Seeds the database
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+    await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
 
-  const story = await Story.bulkCreate(storyData, {
-    individualHooks: true,
-    returning: true,
-  });
-  
-  const submissions = await Submission.bulkCreate(submissionData, {
-    individualHooks: true,
-    returning: true,
-  });
+    await Story.bulkCreate(storyData, {
+        individualHooks: true,
+        returning: true,
+    });
 
-  process.exit(0);
+    await Submission.bulkCreate(submissionData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
 };
 
 seedDatabase();
